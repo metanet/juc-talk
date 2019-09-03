3d27a8d7ac5ab1d333b1713ace5ad3d6a02a1093
Ensar Basri Kahveci <ebkahveci@gmail.com>
2019-09-03 19:27:36 +0300

    Update start script

--- up.sh
+++ up.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+touch log.txt
+
 dir="pet-owner"
 
 (cd $dir && mvn exec:java -Dexec.mainClass=com.hazelcast.juctalk.RunServer > /dev/null &)
 (cd $dir && mvn exec:java -Dexec.mainClass=com.hazelcast.juctalk.RunServer > /dev/null &)
 (cd $dir && mvn exec:java -Dexec.mainClass=com.hazelcast.juctalk.RunServer > /dev/null &)
+
+tail -n 200 -f log.txt
